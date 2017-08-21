1b0cdb180a5741546d3a0f4a49cb5e6d1c822577
Penghao Cen <scorpiocph@gmail.com>
2017-08-21 11:34:32 +0800

    Python sys.path.insert

---
+++ python/src/cph.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+
+def print_str(str):
+    print(str)
---
+++ python/test.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+import sys, os, pdb
+sys.path.insert(0, 'src')
+
+from cph import print_str
+
+def main():
+    print_str('import from src directory ok')
+
+if __name__ == '__main__':
+    main()
